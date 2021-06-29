8cfbb2b7e4975e8d55f772bfeb367e75ab0ccbba
tamburrisam <tamburrisam@gmail.com>
2021-06-29 14:23:35 -0400

    added authStateChanged functions- set up UI and display rooms from db

--- src/index.js
+++ src/index.js
@@ -12,6 +12,27 @@ const signupForm = document.getElementById("signup-form");
 const logOutBtn = document.getElementById("log-out");
 const usernameContainer = document.querySelector("#user");
 const signinForm = document.querySelector("#signin-form");
+const roomList = document.querySelector(".rooms");
+const rooms = document.querySelector("#rooms");
+
+const loggedOutLinks = document.querySelectorAll(".logged-out");
+const loggedInLinks = document.querySelector(".logged-in");
+
+const setUpUI = (user) => {
+  if (user) {
+    //togle UI elements
+    loggedInLinks.style.display = "block";
+
+    loggedOutLinks.forEach((item) => {
+      item.style.display = "none";
+    });
+  } else {
+    loggedInLinks.style.display = "none";
+    loggedOutLinks.forEach((item) => {
+      item.style.display = "block";
+    });
+  }
+};
 
 signupForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -30,6 +51,7 @@ signupForm.addEventListener("submit", (e) => {
   auth.createUserWithEmailAndPassword(email, password).then((cred) => {
     usernameContainer.innerHTML = cred.user.email;
     signupForm.reset();
+    rooms.style.display = "block";
   });
 });
 
@@ -42,6 +64,7 @@ logOutBtn.addEventListener("click", function (e) {
     console.log("user signed out");
   });
   usernameContainer.innerHTML = "Signed Out";
+  rooms.style.display = "none";
 });
 
 //login
@@ -61,5 +84,51 @@ signinForm.addEventListener("submit", (e) => {
     usernameContainer.innerHTML = "Hello " + cred.user.email;
     console.log("logged in");
     signinForm.reset();
+    rooms.style.display = "block";
   });
 });
+
+//keep track of user auth status
+//logged in? logged out?
+//onAuthStateChanged - listens for any authentication sstages
+//listen for auth status changes
+auth.onAuthStateChanged((user) => {
+  console.log(user);
+
+  //will only fire when user logs in
+  //user is true = user exists
+  if (user) {
+    //get data
+    //async task
+    //snapshot digital representation in that moment in tiome
+    rooms.style.display = "block";
+    db.collection("rooms")
+      .get()
+      .then((snapshot) => {
+        console.log("Snapshot docs", snapshot.docs);
+        setUpRooms(snapshot.docs);
+      });
+    setUpUI(user);
+  } else {
+    setUpRooms([]);
+    setUpUI();
+  }
+});
+
+//this takes the snapshot object and iterates through it to get the info
+const setUpRooms = (data) => {
+  //if there is data
+  if (data.length) {
+    let html = "";
+    data.forEach((doc) => {
+      const room = doc.data();
+      console.log("Iterated snapshot", room);
+      const li = `<li><button>${room.Name}</button>${room.Count} Students</li>`;
+
+      html += li;
+    });
+    roomList.innerHTML = html;
+  } else {
+    roomList.innerHTML = `<h5>Log in to view rooms</h5>`;
+  }
+};
